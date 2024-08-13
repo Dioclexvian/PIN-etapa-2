f99481f362c69515b17da9d1eafc9116d31aa69d
dioclexvian <dfsuarezs.ing@gmail.com>
2024-08-13 18:01:42 -0500

    backend s3

--- backend.tf
+++ backend.tf
@@ -1,3 +1,26 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "mundose22"
+  
+  tags = {
+    Name = "terraform_state_bucket"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraformstatelock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraformstatelock"
+  }
+}
+
 terraform {
   backend "s3"{
     bucket                 = "mundose22"
@@ -7,6 +30,9 @@ terraform {
   }
 }
 
-module "bootstrap" {
-  source = "./modules/bootstrap"
+output "bucket_name" {
+  value       = aws_s3_bucket.terraform_state.bucket
+  description = "El nombre del bucket S3 donde se almacena el estado de Terraform."
 }
+
+
